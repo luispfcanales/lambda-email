41b559dc19be23d045d30c6044360cbc8f04bb5d
luispfcanales <luispfcanales@gmail.com>
2022-12-21 18:57:42 -0500

    options

--- api/email.go
+++ api/email.go
@@ -21,7 +21,7 @@ func Email(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	//w.Header().Set("Access-Control-Allow-Credentials", "true")
-	//w.Header().Set("Access-Control-Allow-Methods", "POST,GET, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Methods", "POST,GET, OPTIONS")
 	//w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	w.Header().Set("Content-Type", "application/json")
 
